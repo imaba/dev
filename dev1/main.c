84456cfb5403fe34da7108726df693484b129129
imaba <imabag@gmail.com>
2016-09-25 16:45:28 +0900

    Initial Commit

---
+++ dev1/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  dev1
+//
+//  Created by 今林　毅一郎 on 2016/09/25.
+//  Copyright © 2016年 今林　毅一郎. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
